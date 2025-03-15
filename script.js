0a74326e6766121c0bb5151ddcf6cc66f8707f16
sristy <sristy.sharma98@gmail.com>
2025-03-15 23:54:25 +0530

    first draft

--- script.js
+++
@@ -1,5 +0,0 @@
-// Dark Mode Toggle
-const toggleButton = document.getElementById('dark-mode-toggle');
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
